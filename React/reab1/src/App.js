3a7698d20f323c92e6f50644d70572a260646773
Kartik Padia <kartikbpadia0707@gmail.com>
2025-11-26 02:26:02 +0000

    lab0

--- React/reab1/src/App.js
+++ React/reab1/src/App.js
@@ -1,19 +1,35 @@
-import Name from "./pages/Name.js";
-import Nameclass from "./pages/Nameclass.js";
-import Rollp from "./pages/Rollp.js";
-import Event from "./pages/Event.js";
-function App() {
-  return (
+// LAB 0
+
+// import Name from "./pages/Lab0/Name.js";
+// import Nameclass from "./pages/Lab0/Nameclass.js";
+// import Rollp from "./pages/Lab0/Rollp.js";
+// import Event from "./pages/Lab0/Event.js";
+// function App() {
+//   return (
+//     <>
+//       <header>
+//         <nav>
+//           <div><Name/></div>
+//           <div><Nameclass/><Rollp rollno={247}></Rollp></div>
+//           <Event/>
+//         </nav>
+//       </header>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+// LAB 1
+
+
+
+function App(){
+  return(
     <>
-      <header>
-        <nav>
-          <div><Name/></div>
-          <div><Nameclass/><Rollp rollno={247}></Rollp></div>
-          <Event/>
-        </nav>
-      </header>
+      
     </>
   );
 }
-
 export default App;
